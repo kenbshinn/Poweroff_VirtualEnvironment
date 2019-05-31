5512da4e45f4265b35453a62738307b653aaddab
Ken Shinn <kenbshinn@gmail.com>
2019-05-31 18:00:37 -0400

    Add files via upload
    This is the base script, I would like to add a few additional features such as:
    
    a. Loop to bring you back to the main part of the script
    b. Verification if PowerCLI is installed already before it tries to run the script. 
    
    This should cut down on the amount of time it takes to run the script

---
+++ Power off VMware Lab.ps1
@@ -0,0 +1,173 @@
+##########################
+#
+#  vCenter Shutdown Tool
+#
+#  
+#
+#  #  Ver 0.1
+#  Written by: Kenneth B Shinn Jr.
+#  Created 05/30/19
+#  Updated --
+##########################
+
+
+########################################
+#
+# Import assemblies/modules 
+#
+########################################
+
+Install-Module -Name VMware.PowerCLI
+
+
+
+########################################
+#
+# Create Error Logs
+#
+########################################
+
+
+
+
+########################################
+#
+# Global Parameters 
+#
+########################################
+
+
+
+
+
+
+########################################################################################
+#
+# Function to power off virtual machines
+#
+#
+########################################################################################
+
+Function PowerOff-VirtualMachines
+{
+$vcNames = Read-Host -Prompt 'Input your VCSA Server Here'
+
+Connect-VIServer -Server $vcNames
+Get-VM | Where-Object {$_.powerstate -eq 'PoweredOn'-and $_.Name -notlike '*labvcsa01*'} | select Name | Out-File .\poweredonvms.txt
+}
+$VMList = Get-Content .\poweredonvms.txt
+
+foreach($vmName in $VMList)
+{
+    $vm = Get-VM -Name $vmName
+    if($vm.Guest.State -eq "Running"){
+        Shutdown-VMGuest -VM $vm -Confirm:$false}
+    else{    
+        Stop-VM -VM $vm -confirm:$false
+        write-host "Powering Down VM..."
+      export ".\results.csv"}
+    }
+	
+########################################################################################
+#
+# Function to power off lab virtual hosts
+#
+#x
+########################################################################################
+
+Function PowerOff-VirtualHosts
+
+{
+$vcNames = Read-Host -Prompt 'Input your VCSA Server Here'
+Connect-VIServer -Server $vcNames
+
+$vhnames = Read-Host -Prompt 'Input the Virtual Host you would like to power off (example: phlvh00a.domain.local,phlvh00b.domain.local)'
+Stop-VMHost -server $vhnames -Confirm}
+
+
+########################################################################################
+#
+# Function to power off labVCSA01
+#
+#
+########################################################################################
+
+Function PowerOff-LabVCSA01
+{
+ Connect-VIServer -Server 172.26.96.44
+ Shutdown-VMGuest -VM labvcsa01.pcn.local -Confirm:$false
+ }
+ ########################################################################################
+#
+# Function to power off Infrastructure Virtual Host
+#
+#
+########################################################################################
+
+Function PowerOff-InfrastructureHosts
+{
+Connect-VIServer -Server 172.26.96.44
+Stop-VMHost 172.26.96.44 -Confirm
+}
+########################################################################################
+#
+# Function to Report powered on virtual machines Only
+#
+#
+########################################################################################
+
+Function Report-PoweredOnVMs
+{
+$vcNames = Read-Host -Prompt 'Input your VCSA Server Here'
+
+Connect-VIServer -Server $vcNames
+Get-VM | Where-Object {$_.powerstate -eq 'PoweredOn'-and $_.Name -notlike '*labvcsa01*'} | select Name | Out-File C:\temp\vmware\scripts\poweredonvms.txt
+}
+
+
+ ###########################################
+###########################################
+# 
+# Begin Script
+#
+###########################################
+###########################################
+
+Write-Host " "
+Write-Host "     ***********************************" 		-ForegroundColor White
+Write-Host "               Lab Power Down"			        -ForegroundColor White
+Write-Host "               "			        -ForegroundColor White
+Write-Host "               Created 05/30/19"			        -ForegroundColor White
+Write-Host "     ***********************************"		-ForegroundColor White
+Write-Host " "
+Write-Host "       Select your specific operation" 	        -ForegroundColor White
+Write-Host " "
+Write-Host " "
+Write-Host "         *** Power Down the Lab***"
+Write-Host " "
+Write-Host "          1.  Power off Virtual Machines"                -ForegroundColor Cyan
+Write-Host " "
+Write-Host "          2.  Power off Virtual Hosts"       -ForegroundColor Green
+Write-Host " "
+Write-Host " "
+Write-Host "      *** Power Down Infrastructure Hosts ***"
+Write-Host " "
+Write-Host "          3.  Power Off LabVCSA01"       -ForegroundColor Magenta
+Write-Host " "
+Write-Host "          4.  Power Off Infrastructure Hosts"       -ForegroundColor Yellow
+Write-Host " "
+Write-Host "    *** Reports ***  "
+Write-Host " "
+Write-Host "          5.  Powered On Virutal Machines"       -ForegroundColor DarkGreen
+Write-Host " "
+Write-Host " "
+$strResponse = Read-Host "Enter Menu Option..."
+If($strResponse -eq "1"){. PowerOff-VirtualMachines}
+	elseif($strResponse -eq "2"){. PowerOff-VirtualHosts}
+    	elseif($strResponse -eq "3"){. PowerOff-LabVCSA01}
+	elseif($strResponse -eq "4"){. PowerOff-InfrastructureHosts}
+    elseif($strResponse -eq "5"){. Report-PoweredOnVMs}
+		else{Write-Host "You did not supply a correct response, `
+		Please run script again." -foregroundColor Red}
+
+		
